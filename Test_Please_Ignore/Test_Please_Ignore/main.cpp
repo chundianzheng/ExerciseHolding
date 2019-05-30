bbabbf1ab183e14523f26d3ddcab77235a84d6e7
郑纯典 <chundianzheng@163.com>
2019-05-30 20:05:25 +0800

    I just want to know where will my comments show in the GitHub website.

---
+++ Test_Please_Ignore/Test_Please_Ignore/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Test_Please_Ignore
+//
+//  Created by Chundian Zheng on 5/30/19.
+//  Copyright © 2019 Chundian Zheng. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
